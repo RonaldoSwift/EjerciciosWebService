8a9719d048d430c738d62336d44d1ffca8868fcd
Ronaldo Vargas <ronaldovh99@gmail.com>
2023-01-31 17:28:04 -0500

    Initial Commit

---
+++ EjerciciosWebService/EjerciciosWebServiceApp.swift
@@ -0,0 +1,20 @@
+//
+//  EjerciciosWebServiceApp.swift
+//  EjerciciosWebService
+//
+//  Created by Ronaldo on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EjerciciosWebServiceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
