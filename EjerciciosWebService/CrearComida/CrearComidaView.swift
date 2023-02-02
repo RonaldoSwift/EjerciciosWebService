fcf5e01c5f61393d0364dd83cca0ef5aab20aefb
Ronaldo Vargas <ronaldovh99@gmail.com>
2023-02-02 18:04:27 -0500

    Ejecutando las pantallas CrearComida y Actualizar Comida (si crea y si elimina)

--- EjerciciosWebService/CrearComida/CrearComidaView.swift
+++ EjerciciosWebService/CrearComida/CrearComidaView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct CrearComidaView: View {
+    
+    var crearComidaViewModel: CrearComidaViewModel = CrearComidaViewModel()
+    @State private var random = Int.random(in: 1..<300)
     @State private var nombre: String = ""
     @State private var precio: String = ""
     @State private var url: String = ""
+    @State private var mensajeDeComidaCreada: String = ""
+    @State private var mostrarAlerta:Bool = false
     
     var body: some View {
         VStack{
@@ -51,7 +56,16 @@ struct CrearComidaView: View {
             
             
             Button {
-                
+                Task{
+                    do{
+                        let comidaCreada = try await crearComidaViewModel.crearComida(postComidasRequest: PostComidasRequest.init(id: String("\(random)"), nombre: nombre, precio: Int("\(precio)")!, url: url))
+                        mensajeDeComidaCreada = "Comida Creada: \nID:\(comidaCreada.id) \nNombre:\(comidaCreada.nombre) \nPrecio: \(comidaCreada.precio) \nURL: \(comidaCreada.url)"
+                        mostrarAlerta = true
+                    } catch{
+                        mensajeDeComidaCreada = "No se creo ninguna comida"
+                        mostrarAlerta = true
+                    }
+                }
             } label: {
                 Text("Crear")
             }
@@ -60,6 +74,9 @@ struct CrearComidaView: View {
             .background(Color("Boton"))
             .foregroundColor(Color.white)
             .cornerRadius(10)
+            .alert(mensajeDeComidaCreada, isPresented: $mostrarAlerta) {
+                Button("OK", role: .cancel) { }
+            }
             
             Spacer()
         }
